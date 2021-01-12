abbddd9815134e8e8877d49a3b2c83eaa76149f0
jayden <jayden@cumallover.me>
2021-01-12 18:21:41 +0000

    begin lag fix

---
+++ Blox Saber Editor/Options.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sound_Space_Editor
+{
+    static class Options
+    {
+        public static List<int> color1 = new List<int> { 255, 0, 0 };
+        public static List<int> color2 = new List<int> { 0, 255, 255 };
+        private static void Save(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            var lines = new List<string> { "uwu" };
+
+            File.WriteAllLines(path,lines)
+        }
+        private static void Load(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Save(path)
+            }
+            else
+            {
+                var lines = File.ReadAllLines(path);
+                
+            }
+        }
+    }
+}
